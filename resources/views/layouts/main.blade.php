1309f4f439dd666ce66a7f1a5352b557f9d6bca1
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-27 17:26:49 +0100

    rendered trains on page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,28 @@
 </head>
 <body>
     <h1 class="text-center text-danger py-4">
-        Trains 
+        Trains: 
     </h1>
+
+    <ul class="list-group d-flex justify-content-center align-items-center flex-wrap">
+        @foreach ($trains as $train)
+            <li>
+                <ul class="mb-4 list-group text-center">
+                    <li class="list-group-item">{{$train->Azienda}}</li>
+                    <li class="list-group-item">{{$train->Stazione_di_partenza}}</li>
+                    <li class="list-group-item">{{$train->Stazione_di_arrivo}}</li>
+                    <li class="list-group-item">{{$train->Orario_di_partenza}}</li>
+                    <li class="list-group-item">{{$train->Orario_di_arrivo}}</li>
+                    <li class="list-group-item">{{$train->Codice_treno}}</li>
+                    <li class="list-group-item"> {{$train->Carrozze}}</li>
+                    <li class="list-group-item"> @if ($train->In_orario)             
+                        {{$train->In_orario = 'Il treno è in orario'}}         
+                        @else {{$train->In_orario = 'Il treno è in ritardo'}}
+                    @endif
+                    </li>
+                </ul>
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
